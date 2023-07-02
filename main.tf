343ab779da2b64cf1e63599126abe9f9c346b055
H3rMajesty <108346788+H3rMajesty@users.noreply.github.com>
2023-07-02 15:16:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.75.0"
-    }
-  }
-}
+resource "aws_instance" "myec22" {
+    ami = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
 
-provider "aws" {
-  region = "us-west-2"
-}
-
-variable "bucketname" {
-  type    = string
-  default = "env0-acme-bucket-new"
-}
-
-resource "random_string" "random" {
-  length  = 5
-  special = false
-  lower   = true
-  upper   = false
-  number  = true
-}
-
-module "s3" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.0.0"
-
-  bucket = "${var.bucketname}-${random_string.random.id}"
-  acl    = "private"
-}
   
-output "s3_bucket_arn" {
-  value = module.s3.s3_bucket_arn
 }
