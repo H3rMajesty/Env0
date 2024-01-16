062cc2f928a367816212618741864e3611bb4c7e
H3rMajesty <108346788+H3rMajesty@users.noreply.github.com>
2024-01-16 17:59:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-east-1"
 }  
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "myec222" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
 
